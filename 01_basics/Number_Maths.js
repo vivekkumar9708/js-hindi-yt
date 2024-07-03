0cf56dc65e08a7b8b309f0136a65afea8f154458
Vivek Kumar Jha <47412957+vivekkumar9708@users.noreply.github.com>
2024-07-03 09:49:14 +0000

    Num and Maths

---
+++ 01_basics/Number_Maths.js
@@ -0,0 +1,25 @@
+// ******************************Number*************************************
+
+const score = 400
+console.log(score);
+
+const balance = new Number(20)//Define number
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+
+// ************************Maths***********************************
+//Method and Funtions
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.round(4.5));
+console.log(Math.floor(4.5));
+console.log(Math.min(4.5, 5, 9, 10));
+console.log(Math.max(4.5, 3, 5, 1, 0));
+console.log(Math.random());//Random value in this method
+
+
+
+
