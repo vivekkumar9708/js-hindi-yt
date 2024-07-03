96dac0838d1b560549c376edd52149263059fa6b
Vivek Kumar Jha <47412957+vivekkumar9708@users.noreply.github.com>
2024-07-03 07:28:47 +0000

    conversion

---
+++ 01_basics/Conversion.js
@@ -0,0 +1,20 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+//Convert string to number/bool/
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+//Conversion
+//"33" => 33
+//"33abc" => Nan
+//true => 1
+//false => 0
+
+
+let isLoogectIn = 1
+let booleanIsLoogedIn = Boolean(isLoogectIn)
+console.log(booleanIsLoogedIn);
