5b9a6865a8e31b3a8c11011d2acfa5e725d2c1fe
Vivek Kumar Jha <47412957+vivekkumar9708@users.noreply.github.com>
2024-07-04 15:44:58 +0000

    Array

---
+++ 02_Basics/Array.js
@@ -0,0 +1,9 @@
+// Array in JavaScript
+const myArr = [0, 1, 2, 3, 4]
+const myArr2 = ["Vivek", "Jha", "Kumar"]
+const myArr3 = new Array(1,2,3,"Vivek")
+console.log(myArr3);
+console.log(myArr3[3]);
+
+
+//Arrray Methods
